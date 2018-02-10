bbfd03e3fbb4fb69ccc28e6128f6698ba3f9afc4
Leo Schweizer <leonhard.schweizer@gmail.com>
2018-02-10 20:18:10 +0100

    lint fixes

--- src/middleware.js
+++ src/middleware.js
@@ -10,13 +10,14 @@ const middleware = (options = {}) => store => {
 	return next => async action => {
 
 		if (action.type === constants.SYNC) {
+			store.dispatch({ type: constants.SYNC_PENDING });
 			try {
 				const [space, contentTypes, syncResult] = await Promise.all([
 					client.getSpace(),
 					client.getContentTypes(),
 					client.sync({ initial: true, resolveLinks: false })
 				]);
-				console.log(space, contentTypes, syncResult)
+				console.log(space, contentTypes, syncResult);
 			} catch (err) {
 				console.log(err);
 			}
