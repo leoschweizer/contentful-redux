f42c6addc38f6fd75005061c8a733323637f095e
Leo Schweizer <leonhard.schweizer@gmail.com>
2018-02-11 12:55:29 +0100

    don't defer returning from middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,35 @@
 const constants = require('./constants');
 
+const handleContentfulActions = async (client, action, store, options) => {
+
+	const state = options.stateSelector(store.getState());
+
+	if (action.type === constants.SYNC && action.spaceId === options.space) {
+		store.dispatch({ type: constants.SYNC_PENDING, spaceId: options.space });
+		try {
+			const [space, contentTypes, syncResult] = await Promise.all([
+				client.getSpace(),
+				client.getContentTypes({ limit: 1000 }),
+				client.sync({ initial: Boolean(!state.nextSyncToken), nextSyncToken: state.nextSyncToken, resolveLinks: false })
+			]);
+			store.dispatch({
+				type: constants.SYNC_FINISHED,
+				spaceId: options.space,
+				space,
+				contentTypes: contentTypes.toPlainObject().items,
+				...syncResult.toPlainObject()
+			});
+		} catch (err) {
+			store.dispatch({
+				type: constants.SYNC_FAILED,
+				spaceId: options.space,
+				error: err.toString()
+			});
+		}
+	}
+
+};
+
 const makeMiddleware = options => store => {
 
 	const client = options.createClient({
@@ -7,38 +37,10 @@ const makeMiddleware = options => store => {
 		accessToken: options.accessToken
 	});
 
-	return next => async action => {
-
-		const state = options.stateSelector(store.getState());
-
+	return next => action => {
 		const result = next(action);
-
-		if (action.type === constants.SYNC && action.spaceId === options.space) {
-			store.dispatch({ type: constants.SYNC_PENDING, spaceId: options.space });
-			try {
-				const [space, contentTypes, syncResult] = await Promise.all([
-					client.getSpace(),
-					client.getContentTypes({ limit: 1000 }),
-					client.sync({ initial: Boolean(!state.nextSyncToken), nextSyncToken: state.nextSyncToken, resolveLinks: false })
-				]);
-				store.dispatch({
-					type: constants.SYNC_FINISHED,
-					spaceId: options.space,
-					space,
-					contentTypes: contentTypes.toPlainObject().items,
-					...syncResult.toPlainObject()
-				});
-			} catch (err) {
-				store.dispatch({
-					type: constants.SYNC_FAILED,
-					spaceId: options.space,
-					error: err.toString()
-				});
-			}
-		}
-
+		handleContentfulActions(client, action, store, options);
 		return result;
-
 	};
 
 };
